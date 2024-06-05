8cb25918c77d295a8300344c3284881a8029044a
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-06-05 22:05:17 +0200

    Add orchestrator service module

---
+++ OrchestratorServce/src/main/java/rs/ac/uns/acs/nais/orchestratorservce/OrchestratorServceApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.acs.nais.orchestratorservce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorServceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestratorServceApplication.class, args);
+    }
+
+}
---
+++ OrchestratorServce/src/test/java/rs/ac/uns/acs/nais/orchestratorservce/OrchestratorServceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.acs.nais.orchestratorservce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
