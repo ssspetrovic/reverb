9b2e7c627554756d2cb9459f468f81e3336c122b
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-06-06 22:48:58 +0200

    Add models for the users in neo4j

---
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/controller/UserController.java
@@ -0,0 +1,64 @@
+package rs.ac.uns.acs.nais.GraphDatabaseService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.uns.acs.nais.GraphDatabaseService.model.Song;
+import rs.ac.uns.acs.nais.GraphDatabaseService.model.User;
+import rs.ac.uns.acs.nais.GraphDatabaseService.service.IUserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final IUserService userService;
+
+    @Autowired
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserByUserId(@PathVariable Long userId) {
+        Optional<User> user = userService.getUserById(userId);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User user) {
+        if (!userId.equals(user.getUserId())) {
+            return ResponseEntity.badRequest().build();
+        }
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{userId}/favorite-songs")
+    public ResponseEntity<List<Song>> getAllFavoriteSongs(@PathVariable Long userId) {
+        List<Song> favoriteSongs = userService.getAllFavoriteSongs(userId);
+        return ResponseEntity.ok(favoriteSongs);
+    }
+}
