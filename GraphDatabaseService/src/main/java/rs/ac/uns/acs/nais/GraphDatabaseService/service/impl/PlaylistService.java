ecbacaa1088cd2daff3831deaba65714ecf8a013
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-06-06 23:08:41 +0200

    Fix postgres import script

--- GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/service/impl/PlaylistService.java
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/service/impl/PlaylistService.java
@@ -1,4 +1,4 @@
-package rs.ac.uns.acs.nais.GraphDatabaseService.service;
+package rs.ac.uns.acs.nais.GraphDatabaseService.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,6 +7,7 @@ import rs.ac.uns.acs.nais.GraphDatabaseService.repository.PlaylistRepository;
 import rs.ac.uns.acs.nais.GraphDatabaseService.dto.PlaylistGenreCountDTO;
 import rs.ac.uns.acs.nais.GraphDatabaseService.dto.PlaylistCountAveragePopularityDTO;
 import rs.ac.uns.acs.nais.GraphDatabaseService.dto.Top4SubgenresPerGenreDTO;
+import rs.ac.uns.acs.nais.GraphDatabaseService.service.IPlaylistService;
 
 import java.util.List;
 
