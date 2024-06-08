34697c546bb3a16fc8bc1f5e8d68b2868e7d7536
dusaan21 <dusan.a.stojanovic@gmail.com>
2024-06-08 17:36:27 +0200

    Bugfox

--- ElasticSearchDatabaseService/src/main/java/rs/ac/uns/acs/nais/ElasticSearchDatabaseService/ElasticSearchDatabaseServiceApplication.java
+++ ElasticSearchDatabaseService/src/main/java/rs/ac/uns/acs/nais/ElasticSearchDatabaseService/ElasticSearchDatabaseServiceApplication.java
@@ -4,9 +4,11 @@ package rs.ac.uns.acs.nais.ElasticSearchDatabaseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableElasticsearchRepositories(basePackages = "rs.ac.uns.acs.nais.ElasticSearchDatabaseService.repository")
 public class ElasticSearchDatabaseServiceApplication {
 
 	public static void main(String[] args) {
--- ElasticSearchDatabaseService/src/main/java/rs/ac/uns/acs/nais/ElasticSearchDatabaseService/model/Playlist.java
+++ ElasticSearchDatabaseService/src/main/java/rs/ac/uns/acs/nais/ElasticSearchDatabaseService/model/Playlist.java
@@ -14,9 +14,9 @@ public class Playlist {
     @Id
     private String Id;
 
-    private String playlist_name;
-    private String playlist_genre;
-    private String playlist_subgenre;
+    private String name;
+    private String genre;
+    private String subgenre;
 
     @Field(type = FieldType.Nested, includeInParent = true)
     private List<Track> tracks;
@@ -25,43 +25,6 @@ public class Playlist {
 
     }
 
-    public String getPlaylist_subgenre() {
-        return playlist_subgenre;
-    }
 
-    public void setPlaylist_subgenre(String playlist_subgenre) {
-        this.playlist_subgenre = playlist_subgenre;
-    }
 
-    public String getPlaylist_genre() {
-        return playlist_genre;
-    }
-
-    public void setPlaylist_genre(String playlist_genre) {
-        this.playlist_genre = playlist_genre;
-    }
-
-    public String getPlaylist_name() {
-        return playlist_name;
-    }
-
-    public void setPlaylist_name(String playlist_name) {
-        this.playlist_name = playlist_name;
-    }
-
-    public List<Track> getTracks() {
-        return tracks;
-    }
-
-    public void setTracks(List<Track> tracks) {
-        this.tracks = tracks;
-    }
-
-    public String getId() {
-        return Id;
-    }
-
-    public void setId(String id) {
-        Id = id;
-    }
 }
