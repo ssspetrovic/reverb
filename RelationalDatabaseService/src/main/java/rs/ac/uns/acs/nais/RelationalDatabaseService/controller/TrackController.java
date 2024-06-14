9cf8ca4eb5a6f60e466fd3d961c0514d22b49c21
MatejMihailovic <mihailovicmatej@gmail.com>
2024-06-14 15:29:00 +0200

    Finalne ispravke

--- RelationalDatabaseService/src/main/java/rs/ac/uns/acs/nais/RelationalDatabaseService/controller/TrackController.java
+++ RelationalDatabaseService/src/main/java/rs/ac/uns/acs/nais/RelationalDatabaseService/controller/TrackController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/tracks")
+@RequestMapping("a")
 public class TrackController {
 
     private final TrackService trackService;
